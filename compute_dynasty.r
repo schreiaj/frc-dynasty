e817332781a332477d9d6b2919b11056ef6cdd9b
Andrew Schreiber <schreiaj@Andrews-MacBook-Pro.local>
2016-12-14 10:00:16 -0500

    computing scores, still have unofficial events in causing erroneous teams

--- compute_dynasty.r
+++ compute_dynasty.r
@@ -8,7 +8,16 @@ c("qm" = 0.0000,
                 "sf" = 0.2500,
                 "f" = 0.5000)-> lookup
 
-all_data$points = lookup[all_data$level]
+lookup = as.data.frame(lookup)
+rownames(lookup) -> lookup$level
+inner_join(all_data, lookup, 'level') -> all_data
+
 
 # Lower bound points for now, todo compute event winner points
-all_data %>% filter(win==T) %>% group_by(year, event, team) %>% summarise(points = max(points)) -> earnings
+all_data %>% group_by(year, event, team, level) %>% summarise(wins = sum(win), points = max(lookup)) -> earnings
+earnings$total_points = earnings$points * earnings$wins
+earnings  %>% group_by(year, event, team) %>% summarise(dynasty = max(total_points)) -> dynasty
+dynasty %>% group_by(year, team) %>% summarise(dynasty_score = mean(dynasty), dev=sd(dynasty, na.rm = T)) -> year_dynasty
+
+team_dynasty <- year_dynasty %>% group_by(team) %>% summarise(score = mean(dynasty_score), var=sd(dev))
+write.csv(team_dynasty, "dynasty_scores.csv", row.names = F)
